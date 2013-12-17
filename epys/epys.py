9f100507868147d816e1f4c86962b966632a4137
Jonathan McAuliffe <watch.n.learn@gmail.com>
2013-12-17 11:40:18 -0600

    converted # comments to docstring... not sure yet if this is a good idea...

--- epys/epys.py
+++ epys/epys.py
@@ -9,43 +9,54 @@ import logging
 
 def read(fname, metadata=False):
     """
-       Reads an EPS generated data-rate file and returns the data
-       in a numpy array.
-       The file metadata can also be returned if requested
+    This function reads an EPS generated data-rate file and returns the data
+    in a numpy array.  The file metadata can also be returned if requested.
+
+    :param fname: The path to the data_rate_avg.out
+    :type fname: str.
+    :param metadata: Flag to return the metadata dictionary
+    :type state: bool.
+    :returns:  numpy.array -- the return code.
     """
 
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
     mdata = {}
-
-    # fh = iter(open(fname, 'U'))
     post_process = False
     hdings = []
 
     with open(fname, 'r') as fh:
         for line in fh:
 
-            # Catch the header data and store it in dictionary.
+            """
+            Catch the header data and store it in dictionary.
+            """
             if re.match( r'#(.*):(.*)', line, re.M|re.I):
                 keypair = line.strip('#\n').split(':')
                 mdata[keypair[0].strip()] = keypair[1].strip()
                 continue
 
-            # Catch the reference date and add to dictionary.
+            """
+            Catch the reference date and add to dictionary.
+            """
             if re.match( r'Ref_date:(.*)', line, re.M|re.I):
                 keypair = line.strip('#\n').split(':')
                 mdata['Reference Date'] = keypair[1].strip()
                 ref_date = datetime.strptime(mdata['Reference Date'], "%d-%b-%Y")
                 continue
 
-            # Catch the experiment names to list.
+            """
+            Catch the experiment names to list.
+            """
             if re.match( r'(.*)\<(.*)\>', line, re.M|re.I):
                 xprmnts = [i.replace('<','').replace('_','-') for i in line.replace('.','').replace('>','').split()]
                 continue
 
-            # Catch the column headers and prefix them with
-            # experiment list.
+            """
+            Catch the column headers and prefix them with
+            experiment list.
+            """
             if re.match( r'Elapsed time(.*)', line, re.M|re.I):
                 _hdings = line.split()
                 _hdings[0:2] = [' '.join(_hdings[0:2])]
@@ -60,7 +71,9 @@ def read(fname, metadata=False):
                     x = x + 1
                 continue
 
-            # Catch the units line and process ...
+            """
+            Catch the units line and process ...
+            """
             if re.match( r'ddd_hh:mm:ss(.*)', line, re.M|re.I):
                 _units = line.replace('(','').replace(')','').split()
                 units = _units[0:2]
@@ -74,23 +87,31 @@ def read(fname, metadata=False):
                 continue
 
             if post_process:
-                # Raise an error if the the length of 'units' is not
-                # equal to the length of '_hdings'.
+                """
+                Raise an error if the the length of 'units' is not
+                equal to the length of '_hdings'.
+                """
                 if len(_hdings) != len(units):
                     print('ERROR: The number of headings does not match the number of units!')
 
-                # Pair the headings and the units ...
+                """
+                Pair the headings and the units ...
+                """
                 for i in range(len(_hdings)):
                     logger.debug('index: %d', i)
                     logger.debug('size of _hdings: %d', len(_hdings))
                     logger.debug('size of units: %d', len(units))
                     hdings.append({'head':_hdings[i], 'unit':units[i]})
 
-                # Prepare
+                """
+                Prepare 'data' array...
+                """
                 data = np.array([x['head'] for x in hdings])
                 post_process = False
 
-            # Check for start of data
+            """
+            Check for start of data
+            """
             if re.match( r'[0-9]{3}_[0-9]{2}:[0-9]{2}:[0-9]{2}(.*)', line, re.M|re.I):
                 days_time = line.split()[0]
                 _data = [float(x) for x in line.split()[1:]]
@@ -112,7 +133,7 @@ def read(fname, metadata=False):
 
 def main():
     """
-        If epys.py is run this main function will be executed.
+    If epys.py is run this main function will be executed.
     """
 
     test = read("/Users/jmcaulif/Code/python/epys/resources/sample_data/data_rate_avg.out")
