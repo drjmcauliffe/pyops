ea0c9b85a0dc4cb99cb002c10bd316dfa8652c50
Jonathan McAuliffe <watch.n.learn@gmail.com>
2013-12-17 23:45:15 -0600

    data_rate_file location updated and tests for the demo function added.

--- tests/test_epys.py
+++ tests/test_epys.py
@@ -17,7 +17,7 @@ class TestEpys(unittest.TestCase):
 
 
     def setUp(self):
-        self.data_rate_file = "./resources/sample_data/data_rate_avg.out"
+        self.data_rate_file = "./sample_data/data_rate_avg.out"
 
     def test_epys_read_data_rate_data_shape(self):
         data, meta = epys.read(self.data_rate_file, metadata=True)
@@ -25,6 +25,14 @@ class TestEpys(unittest.TestCase):
 
     def test_epys_read_data_rate_meta_length(self):
         data, meta = epys.read(self.data_rate_file, metadata=True)
+        assert len(meta) == 16
+
+    def test_epys_demo_data_rate_data_shape(self):
+        data, meta = epys.demo()
+        assert data.shape == (367, 53)
+
+    def test_epys_demo_data_rate_meta_length(self):
+        data, meta = epys.demo()
         assert len(meta) == 16
 
     def tearDown(self):
