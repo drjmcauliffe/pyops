ea725a9b40b4c561b784cdd3b7c216ca6d672b67
Jonathan McAuliffe <watch.n.learn@gmail.com>
2013-12-17 22:23:28 -0600

    excessive blank lines removed

--- tests/test_epys.py
+++ tests/test_epys.py
@@ -15,17 +15,14 @@ from epys import epys
 
 class TestEpys(unittest.TestCase):
 
-
     def setUp(self):
         pass
-
 
     def test_epys_read(self):
         samplefile = "./resources/sample_data/data_rate_avg.out"
         data, meta = epys.read(samplefile, metadata=True)
 
         assert data.shape == (367, 53)
-
 
     def tearDown(self):
         pass
