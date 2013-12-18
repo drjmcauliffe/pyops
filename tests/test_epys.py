419d9f6fd1b6a0102600af7bf54850f7d15dd909
Jonathan McAuliffe <watch.n.learn@gmail.com>
2013-12-17 22:37:53 -0600

    setup function given content. test function split in 2: 1 for data.shape and the other for len(data)

--- tests/test_epys.py
+++ tests/test_epys.py
@@ -15,14 +15,17 @@ from epys import epys
 
 class TestEpys(unittest.TestCase):
 
+
     def setUp(self):
-        pass
+        self.data_rate_file = "./resources/sample_data/data_rate_avg.out"
 
-    def test_epys_read(self):
-        samplefile = "./resources/sample_data/data_rate_avg.out"
-        data, meta = epys.read(samplefile, metadata=True)
-
+    def test_epys_read_data_rate_data_shape(self):
+        data, meta = epys.read(self.data_rate_file, metadata=True)
         assert data.shape == (367, 53)
+
+    def test_epys_read_data_rate_meta_length(self):
+        data, meta = epys.read(self.data_rate_file, metadata=True)
+        assert len(meta) == 16
 
     def tearDown(self):
         pass
