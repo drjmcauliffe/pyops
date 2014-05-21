451b230ad77f395093c091eddad1dda0fd64aa2c
Jonathan McAuliffe <watch.n.learn@gmail.com>
2014-05-21 10:59:36 +0200

    docs/conf.py updated to allow different local and readthedocs builds of the docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,14 @@
 # serve to show the default.
 
 import sys, os
-import sphinx_rtd_theme
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
