2e898a8d9d7e1514fed26a8f1a5881acab5cbc7d
Jonathan McAuliffe <watch.n.learn@gmail.com>
2014-07-21 03:06:13 +0200

    explicit imports list started in __init__. classes file started

--- epys/__init__.py
+++ epys/__init__.py
@@ -13,4 +13,4 @@ __email__ = 'watch.n.learn@gmail.com'
 __version__ = '0.3.1'
 __url__ = 'https://github.com/johnnycakes79/epys'
 
-from epys.read import *
+from epys.read import datarate
---
+++ epys/classes.py
@@ -0,0 +1,17 @@
+class edf:
+
+    def __init__(self, name):
+        print("this is my first class...")
+        self.name = name
+
+
+class erf:
+    pass
+
+
+class evf:
+    pass
+
+
+class itl:
+    pass
