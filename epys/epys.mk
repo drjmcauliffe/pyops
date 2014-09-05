f0aa1f2005661b9fa0f8045bd12428129c16d2e7
Jonathan McAuliffe <watch.n.learn@gmail.com>
2014-09-05 09:24:54 +0200

    paths updated for new local file structure.

--- epys/epys.mk
+++ epys/epys.mk
@@ -1,17 +1,17 @@
 KPL/MK
 \begindata
 
- PATH_VALUES     = ( '/Users/jmcaulif/Code/naif' )
+ PATH_VALUES     = ( '/Users/jmcaulif/Code/naif/bepi/misc/kernels' )
  PATH_SYMBOLS = ( 'KERNELS' )
  KERNELS_TO_LOAD = (
-      '$KERNELS/generic/lsk/naif0010.tls',
-      '$KERNELS/generic/spk/de430.bsp',
-      '$KERNELS/generic/pck/pck00010.tpc',
-      '$KERNELS/bepi/sclk/bc_sclkscet_00008_science.tsc',
-      '$KERNELS/bepi/spk/MPO_8848MJD_HiRes_480x1500.bsp',
-      '$KERNELS/bepi/fk/mpo_v0001.tf',
-      '$KERNELS/bepi/ik/mpo_mertis_v0001.ti',
-      '$KERNELS/bepi/ck/mpo_nadir_attitude.bc'
+      '$KERNELS/lsk/naif0010.tls',
+      '$KERNELS/spk/de430.bsp',
+      '$KERNELS/pck/pck00010.tpc',
+      '$KERNELS/sclk/bc_sclkscet_00008_science.tsc',
+      '$KERNELS/spk/MPO_8848MJD_HiRes_480x1500.bsp',
+      '$KERNELS/fk/mpo_v0001.tf',
+      '$KERNELS/ik/mpo_mertis_v0001.ti',
+      '$KERNELS/ck/mpo_nadir_attitude.bc'
        )
 
 \begintext
