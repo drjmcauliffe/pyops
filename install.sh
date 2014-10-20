5de259cd83b00fcd8250270b068dfa9c83918a76
Jonathan McAuliffe <watch.n.learn@gmail.com>
2014-10-20 18:31:59 +0200

    install script built and requirements.txt updated

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# install the pypi requirements.
+pip install -r requirements.txt
+
+# install PyAstronomy - needs Numpy to be already installed.
+cd ..
+git clone https://github.com/sczesla/PyAstronomy.git
+cd PyAstronomy
+python setup.py --with-ext install
+cd ..
+
+# install svgFig for orbital graphics
+wget http://svgfig.googlecode.com/files/svgfig-1.1.2.tgz
+tar -xvf svgfig-1.1.2.tgz
+rm svgfig-1.1.2.tgz
+cd svgfig
+python setup.py install
+cd ..
+
+# install PySpice - for now! Soon to be updated to SpiceyPy
+git clone https://github.com/johnnycakes79/PySPICE.git
+cd PySpice
+python getspice.py
+python setup.py install
+cd ..
+
+# finally install e[PY]s
+cd epys
+python setup.py install
+python setup.py test # run tests to check basic functionality
+
+# run an ipython notebook to see if things work
+ipython notebook
