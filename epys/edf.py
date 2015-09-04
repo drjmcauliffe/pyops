aa9c9e3fb78128058662513826ab847364b0d8ed
Pablo Lanaspa Ferrer <planaspa@gmail.com>
2015-09-04 10:44:39 +0200

    Now the EDF object is capable of create a table with all the FOVs.

--- epys/edf.py
+++ epys/edf.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 class EDF:
 
     def __init__(self, fname):
@@ -10,15 +13,35 @@ class EDF:
 
         # Tables to fill in order as they appear in the file
         self.DATA_STORES = None
+        self._data_stores = dict()
         self.PIDS = None
+        self._pids = dict()
         self.FTS = None
+        self._fts = dict()
         self.FOV = None
+        self._fov = {"FOV": [], "FOV_lookat": [], "FOV_upvector": [],
+                     "FOV_type": [], "FOV_algorithm": [],
+                     "FOV_geometric_angles": [], "FOV_geometric_pixels": [],
+                     "FOV_sub_view": [], "FOV_straylight_angles": [],
+                     "FOV_straylight_duration": [], "FOV_active": [],
+                     "FOV_image_timing": [], "FOV_imaging": [],
+                     "FOV_pitch": [], "FOV_yaw": []}
         self.AREA = None
+        self._area = dict()
         self.MODULES = None
+        self._modules = dict()
         self.MODES = None
+        self._modules = dict()
         self.PARAMETERS = None
+        self._parameters = dict()
         self.ACTIONS = None
+        self._actions = dict()
         self.CONSTRAINTS = None
+        self._constraints = dict()
+
+        # Keywords to detect in the filed
+        self.keywords = {'FOV': self._read_fov, 'MODULE': self._read_module,
+                         'MODE': self._read_mode}
 
         # Loading the given file
         self.load(fname)
@@ -55,7 +78,11 @@ class EDF:
                 l = line.split()
                 # We have found a variable
                 if ':' in l[0][-1]:
-                    self.variables[l[0][:-1]] = l[1:]
+                    # Checking if we have already found a keyword
+                    if l[0][-1].upper() in self.keywords:
+                        break
+                    else:
+                        self.variables[l[0][:-1]] = l[1:]
                 # We have found a comment
                 elif '#' in l[0][0]:
                     self.WTF.append(line)
@@ -65,47 +92,83 @@ class EDF:
             lines_to_remove += 1
         content = content[lines_to_remove:]
 
-    """
+        pos = 0
+        while pos < len(content):
+            l = content[pos].split()
+            if len(l) > 1 and l[0][:-1].upper() in self.keywords:
+                pos += self.keywords[l[0][:-1].upper()](content[pos:])
+            else:
+                pos += 1
 
-        lines_to_remove = 0
-        # Read Data Stores
-        for line in content:
-
-            # Removing the line from content
-            lines_to_remove += 1
-        content = content[lines_to_remove:]
-        # Read PIDS
-
-
-        # Read FTS
-
-
-        # Read FOV
-
-
-        # Read AREA
-
-
-        # Read MODULES
-
-
-        # Read MODES
-
-
-        # Read PARAMETERES
-
-
-        # Read ACTIONS
-
-
-        # Read CONSTRAINTS
-
-    """
+        # Creating the pandas tables
+        self._convert_dictionaries_into_dataframes()
 
     def _read_metada(self, line):
         if ': ' in line:
             self.meta[line[1:line.index(': ')].strip()] = \
                 line[line.index(': ') + 1:-1].strip()
 
-    def _read_data_stores(self):
-        pass
+    def _read_data_stores(self, content):
+        print "data store detected"
+        return 1
+
+    def _read_fov(self, content):
+        counter = 0
+        line = ""
+        for l in content:
+            # Concatening lines if '\' found
+            if '\\' in l and '#' not in l[0] and \
+               '\\' not in l[l.index('\\') + 1]:
+                line += l[:l.index('\\')]
+                # Continues with the next iteration of the loop
+                counter += 1
+                continue
+
+            # If there was no concatenation of lines
+            if len(line) == 0:
+                line = l
+            # If we were concatenating, we concatenate the last one
+            else:
+                if '#' in l:
+                    line += l[:l.index('#')]
+                else:
+                    line += l
+
+            line = line.split()
+            if len(line) > 1:
+                if line[0][:-1] in self._fov:
+                    if len(line[1:]) == 1:
+                        # If another FOV detected we ensure to keep same length
+                        # of all the elements in the dictionary
+                        if line[0][:-1] == 'FOV':
+                            # Adding None value to the empty fields
+                            maximum = max(
+                                [len(self._fov[x]) for x in self._fov])
+                            for x in self._fov:
+                                if len(self._fov[x]) < maximum:
+                                    self._fov[x].append(None)
+                        self._fov[line[0][:-1]].append(line[1])
+                    else:
+                        self._fov[line[0][:-1]].append(line[1:])
+                else:
+                    # Adding None value to the empty fields
+                    maximum = max([len(self._fov[x]) for x in self._fov])
+                    for x in self._fov:
+                        if len(self._fov[x]) < maximum:
+                            self._fov[x].append(None)
+                    break
+            counter += 1
+            line = ""
+
+        return counter
+
+    def _read_module(self, content):
+        print "module detected"
+        return 1
+
+    def _read_mode(self, content):
+        print "mode detected"
+        return 1
+
+    def _convert_dictionaries_into_dataframes(self):
+        self.FOV = pd.DataFrame(self._fov)
