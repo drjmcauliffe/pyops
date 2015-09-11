3287dbb2d3e88a2123d303645f5c02f73a782e65
Pablo Lanaspa Ferrer <planaspa@gmail.com>
2015-09-11 12:04:29 +0200

    Test file created and EDF tested over it.
    Not all the cells of the whole output are tested but at least one value of
    every table is tested.

--- test/test_edf.py
+++ test/test_edf.py
@@ -1,4 +1,5 @@
 from epys import EDF
+import os
 
 
 def test_add_none_to_empty_fields():
@@ -76,3 +77,52 @@ def test_concatenate_lines():
                     'bcd\\\n', 'aa\\\\\n', 'c\n']
     output = edf_test._concatenate_lines(test_content)
     assert output == ['#abc\\', 'abcd b aa', 'bcd aa\\\\', 'c']
+
+
+def test_load_edf_file():
+    this_dir, this_filename = os.path.split(__file__)
+    parent_dir = os.path.abspath(os.path.join(this_dir, os.pardir))
+    test_file = os.path.join(parent_dir, "test/data/test.edf")
+
+    edf = EDF(test_file)
+
+    assert edf.experiment == 'SSMM "MassMemory"'
+
+    for key in edf.GLOBAL_PROPERTIES:
+        if key == 'Global_actions':
+            assert edf.GLOBAL_PROPERTIES[key] == \
+                'COPY_DV UPDATE_FTS_TABLE ALLOCATE_PID'
+        else:
+            assert edf.GLOBAL_PROPERTIES[key] is None
+
+    assert len(edf.DATA_STORES.Table) == 28
+    assert edf.DATA_STORES.Table.loc[2]['Memory size'] == '1 [Gbits]'
+
+    assert len(edf.PIDS.Table) == 6
+    assert edf.PIDS.Table.loc[3]['Data Store ID'] == '30'
+
+    assert len(edf.FTS.Table) == 8
+    assert edf.FTS.Table.loc[6]['Data Volume'] == '150'
+
+    assert len(edf.FOVS.Table) == 2
+    assert edf.FOVS.Table.loc[1]['FOV_type'] == 'RECTANGULAR'
+
+    assert len(edf.MODES.Table) == 5
+    assert edf.MODES.Table.loc[3]['Nominal_power'] == '0 [Watts]'
+
+    assert len(edf.MODULES.Table) == 4
+    assert edf.MODULES.Table.loc[2]['Module_level'] is None
+
+    assert len(edf.MODULES.Module_states_Table) == 10
+    assert edf.MODULES.Module_states_Table.loc[5]['MS_data_rate_parameter'] ==\
+        'VIHI_DATA_RATE'
+
+    assert len(edf.PARAMETERS.Table) == 11
+    assert edf.PARAMETERS.Table.loc[7]['Eng_type'] == 'REAL'
+
+    assert len(edf.PARAMETERS.Parameter_values_Table) == 6
+    assert edf.PARAMETERS.Parameter_values_Table.loc[3]['Parameter_value'] == \
+        'FTS_ENABLE_FLAG - 1 ENABLE'
+
+    assert len(edf.ACTIONS.Table) == 3
+    assert edf.ACTIONS.Table.loc[1]['Duration'] is None
